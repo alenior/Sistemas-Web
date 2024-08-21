f95f0d7a875aa1397c81409d53187c87dba17a9b
alenior <alenior@gmail.com>
2024-08-20 22:46:10 -0300

    Exibições do BD funcionais.

--- projetoContasAPagar/app.py
+++ projetoContasAPagar/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -24,6 +25,12 @@ class Credor(db.Model):
     def __repr__(self):
         return f'<Credor {self.nome}>'
 
+STATUS_MAP = {
+    'pago': 'PAGO',
+    'a_vencer': 'A VENCER',
+    'vencido': 'VENCIDO'
+}
+
 class ContaAPagar(db.Model):
     __tablename__ = 'conta_a_pagar'
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +40,9 @@ class ContaAPagar(db.Model):
     status_conta = db.Column(db.String(20), nullable=False)
     credor_id = db.Column(db.Integer, db.ForeignKey('credor.id'), nullable=False)
     credor = db.relationship('Credor', backref=db.backref('contas_relacionadas', lazy=True))
+
+    def get_status_display(self):
+        return STATUS_MAP.get(self.status_conta, 'Desconhecido')
 
     def __repr__(self):
         return f'<ContaAPagar {self.descricao}>'
@@ -54,25 +64,36 @@ def get_bills():
 
 # Rota para listar contas por período
 @app.route('/contas-por-periodo', methods=['GET', 'POST'])
-def get_bills_by_period():
+def bills_by_period():
+    contas = []
+
     if request.method == 'POST':
         # Supor que você receba as datas no formato 'YYYY-MM-DD'
-        data_inicio = request.form['data_inicio']
-        data_fim = request.form['data_fim']
-        contas = ContaAPagar.query.filter(ContaAPagar.data_vencimento.between(data_inicio, data_fim)).all()
-        return render_template('bills_by_period.html', contas=contas, data_inicio=data_inicio, data_fim=data_fim)
-    return render_template('bills_by_period.html')
+        data_inicio = request.form.get('data_inicio')
+        data_fim = request.form.get('data_fim')
+
+        if data_inicio and data_fim:
+        # Converter as datas para o formato necessário
+            data_inicio = datetime.strptime(data_inicio, '%Y-%m-%d').date()
+            data_fim = datetime.strptime(data_fim, '%Y-%m-%d').date()
+
+            # Consulta ao banco de dados filtrando pelo período
+            contas = ContaAPagar.query.filter(ContaAPagar.data_vencimento >= data_inicio,
+                                          ContaAPagar.data_vencimento <= data_fim).all()
+
+    return render_template('bills_by_period.html', contas=contas)
 
 # Rota para listar contas por credor
 @app.route('/contas-por-credor', methods=['GET', 'POST'])
 def contas_por_credor():
     if request.method == 'POST':
-        credor_id = request.form['credor_id']
+        credor_id = request.form.get('credor_id') # Correção aqui
         contas = ContaAPagar.query.filter_by(credor_id=credor_id).all()
-        credor = Credor.query.get(credor_id)
-        return render_template('contas_por_credor.html', contas=contas, credor=credor)
-    credores = Credor.query.all()
-    return render_template('contas_por_credor.html', credores=credores)
+        credores = Credor.query.all() # Carregar a lista de credores para o formulário
+        return render_template('contas_por_credor.html', contas=contas, credores=credores)
+    
+    credores = Credor.query.all() # Carregar a lista de credores para o formulário
+    return render_template('contas_por_credor.html', contas=[], credores=credores)
 
 if __name__ == '__main__':
     with app.app_context():
